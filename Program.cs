bd808b61d55d0e146759bfe9a8b27bd5a07fde57
DenisT <denis@tikhonov.pp.ru>
2022-08-16 10:30:04 +0300

    add task13

--- Program.cs
+++ Program.cs
@@ -3,3 +3,12 @@ Console.WriteLine("Введите трёхзначное число");
 int A = Convert.ToInt32(Console.ReadLine());
 string numberTwo = Convert.ToString(A);
 Console.WriteLine("вторая цифра "+numberTwo[1]);
+//Задача №13
+Console.WriteLine("Введите число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string numberTree = Convert.ToString(numbers);
+if (numberTree.Length > 2) {
+    Console.WriteLine("третья цифра "+numberTree[2]);
+} else {
+    Console.WriteLine("третьей цифры нет");
+}
